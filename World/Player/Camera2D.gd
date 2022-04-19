505565cef58d8bc31b1b1f786825d43f358b8a34
Jonathan Green <dev.jonathan.green@gmail.com>
2022-04-19 01:54:52 -0400

    removed camera limitations on character

--- World/Player/Camera2D.gd
+++ World/Player/Camera2D.gd
@@ -4,8 +4,9 @@ onready var topLeft = $Limits/TopLeft
 onready var botttomRight = $Limits/BottomRight
 
 func _ready():
-	limit_top = topLeft.position.y
-	limit_left = topLeft.position.x
-	limit_bottom = botttomRight.position.y
-	limit_right = botttomRight.position.x
+	pass
+	#limit_top = topLeft.position.y
+	#limit_left = topLeft.position.x
+	#limit_bottom = botttomRight.position.y
+	#limit_right = botttomRight.position.x
 
