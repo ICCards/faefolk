747938465daeefc09e7bf0e5504c2d2c1c453801
RAW4RMCS <rmc.sprenkels@gmail.com>
2022-06-23 14:10:24 +0200

    add functions

--- helpers/plug.gd
+++ helpers/plug.gd
@@ -6,8 +6,16 @@ func _input(ev):
 	var window = JavaScript.get_interface("window")
 	if Input.is_key_pressed(KEY_C):
 		window.connect()
-	if Input.is_key_pressed(KEY_SPACE):
-		window.call("sanityCheck").then(_callback_ref)
+	if Input.is_key_pressed(KEY_S):
+		window.sanityCheck().then(_callback_ref)
+	if Input.is_key_pressed(KEY_P):
+		window.createPlayer("humanMale").then(_callback_ref)
+	if Input.is_key_pressed(KEY_A):
+		window.submitActions("watering-15|harvesting-12|hoeing-5").then(_callback_ref)
+	if Input.is_key_pressed(KEY_G):
+		window.getPlayer().then(_callback_ref)
+	if Input.is_key_pressed(KEY_W):
+		window.wieldItem("axe").then(_callback_ref)
 		
 
 func _my_callback(args):
