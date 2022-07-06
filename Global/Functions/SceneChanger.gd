24809a2cddcb66c5579b5186ff9af6e592dc7d94
Jonathan Green <dev.jonathan.green@gmail.com>
2022-07-06 15:35:49 -0400

    WIP

--- Global/Functions/SceneChanger.gd
+++ Global/Functions/SceneChanger.gd
@@ -6,14 +6,15 @@ onready var animation_player = $AnimationPlayer
 onready var black = $Node/Black
 
 func change_scene(path, var sound = ""):
-	if sound == "door":
-		$SoundEffects.stream = Sounds.door_open
-		$SoundEffects.play()
-	animation_player.play("fade")
-	yield(animation_player, "animation_finished")
 	assert(get_tree().change_scene(path) == OK)
-	Input.set_custom_mouse_cursor(preload("res://Assets/mouse cursors/Normal Selects.png"))
-	if sound == "door":
-		$SoundEffects.stream = Sounds.door_close
-		$SoundEffects.play()
-	animation_player.play_backwards("fade")
+#	if sound == "door":
+#		$SoundEffects.stream = Sounds.door_open
+#		$SoundEffects.play()
+#	animation_player.play("fade")
+#	yield(animation_player, "animation_finished")
+#	assert(get_tree().change_scene(path) == OK)
+#	Input.set_custom_mouse_cursor(preload("res://Assets/mouse cursors/Normal Selects.png"))
+#	if sound == "door":
+#		$SoundEffects.stream = Sounds.door_close
+#		$SoundEffects.play()
+#	animation_player.play_backwards("fade")
