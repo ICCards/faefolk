b636c4a6101c142be6dfad8f43ecb7afe31d7cc8
Max Wischusen <maxwischusen@Maxs-MacBook-Pro.local>
2022-07-08 12:20:36 -0400

    scene changing fix

--- Global/Functions/SceneChanger.gd
+++ Global/Functions/SceneChanger.gd
@@ -5,15 +5,39 @@ extends CanvasLayer
 onready var animation_player = $AnimationPlayer
 onready var black = $Node/Black
 
-func change_scene(path, var sound = ""):
-	if sound == "door":
-		$SoundEffects.stream = Sounds.door_open
-		$SoundEffects.play()
+var current_scene = null
+
+func _ready():
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+	
+func goto_scene(path):
+	# This function will usually be called from a signal callback,
+	# or some other function in the current scene.
+	# Deleting the current scene at this point is
+	# a bad idea, because it may still be executing code.
+	# This will result in a crash or unexpected behavior.
+
+	# The solution is to defer the load to a later time, when
+	# we can be sure that no code from the current scene is running:
 	animation_player.play("fade")
 	yield(animation_player, "animation_finished")
-	assert(get_tree().change_scene(path) == OK)
-	Input.set_custom_mouse_cursor(preload("res://Assets/mouse cursors/Normal Selects.png"))
-	if sound == "door":
-		$SoundEffects.stream = Sounds.door_close
-		$SoundEffects.play()
+	call_deferred("_deferred_goto_scene", path)
+
+
+func _deferred_goto_scene(path):
+	# It is now safe to remove the current scene
+	current_scene.free()
+
+	# Load the new scene.
+	var s = ResourceLoader.load(path)
+
+	# Instance the new scene.
+	current_scene = s.instance()
+
+	# Add it to the active scene, as child of root.
+	get_tree().get_root().add_child(current_scene)
+
+	# Optionally, to make it compatible with the SceneTree.change_scene() API.
+	get_tree().set_current_scene(current_scene)
 	animation_player.play_backwards("fade")
