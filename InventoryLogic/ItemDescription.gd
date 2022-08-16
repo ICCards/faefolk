3e561fb692ad951f1ca27c9616b57599ee2a939a
Max Wischusen <maxwischusen@Maxs-MBP.fios-router.home>
2022-08-16 17:06:09 -0400

    getting fish images and categorizing

--- InventoryLogic/ItemDescription.gd
+++ InventoryLogic/ItemDescription.gd
@@ -5,17 +5,6 @@ var width
 var height
 var item_name
 
-#func _ready():
-#	set_description_text(item_name)
-#	yield(get_tree(), "idle_frame")
-#	set_size_of_description($ItemName.rect_size.x)
-#	$GridContainer.rect_size.x = width
-#	$GridContainer/TopRow.rect_size.x = width
-#	$GridContainer/MiddleRow.rect_size.x = width
-#	$GridContainer/BottomRow.rect_size.x = width
-#	$ItemDescription.rect_size.x = (width + 80) * 2
-#	$ItemName.rect_size.x = width
-
 
 func initialize():
 	set_description_text(item_name)
@@ -28,6 +17,7 @@ func initialize():
 	$ItemDescription.rect_size.x = (width * 7) 
 	$ItemName.rect_size.x = width
 
+
 func set_description_text(item):
 	if item:
 		var category = JsonData.item_data[item]["ItemCategory"]
@@ -35,8 +25,7 @@ func set_description_text(item):
 		$ItemCategory.modulate = returnCategoryColor(category)
 		$ItemName.set_text(item[0].to_upper() + item.substr(1,-1))
 		$ItemCategory.set_text(category[0].to_upper() + category.substr(1,-1))
-		#$ItemDescription.set_text(description)
-		$ItemDescription.set_text("descriptionodsjfk jdf jslkj sdlfkjdd  dasdas  asd sa a sads sa a spjsa ;lzm aslknm lksadfnlkn fsdlkn sdnflsn dns dlkns dnn")
+		$ItemDescription.set_text(description)
 		
 
 
