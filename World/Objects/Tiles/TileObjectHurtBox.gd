bce9dfd89d9da02f27aa3bdde97fa0d7e79a7c21
Max Wischusen <maxwischusen@Maxs-MacBook-Pro.local>
2022-09-13 11:44:48 -0400

    building fixes and new doors

--- World/Objects/Tiles/TileObjectHurtBox.gd
+++ World/Objects/Tiles/TileObjectHurtBox.gd
@@ -54,7 +54,7 @@ func set_dimensions():
 		PlayerInventory.chests[id] = {}
 		scale.x = 2.0
 		position = position +  Vector2(16, 0)
-	elif item_name == "workbench":
+	elif item_name == "workbench #1":
 		$InteractiveArea/CollisionShape2D.disabled = false
 		$InteractiveArea.collision_mask = 131072
 		scale.x = 2.0
