a29f9cc5d60c2c2e06b8498cf4bf392cef1c20ec
Jonathan Green <dev.jonathan.green@gmail.com>
2022-09-17 11:57:45 -0400

    WIP

--- helpers/ic.cs
+++ helpers/ic.cs
@@ -3,8 +3,11 @@ using System;
 using Newtonsoft;
 using EdjCase.ICP.Agent.Agents;
 using EdjCase.ICP.Agent.Auth;
+using EdjCase.ICP.Agent.Identity;
 using EdjCase.ICP.Candid.Models;
-
+using HDWallet.Core;
+using HDWallet.Ed25519.Sample;
+using NBitcoin;
 public class ic : Node
 {
 	// Declare member variables here. Examples:
@@ -14,9 +17,14 @@ public class ic : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var identity = new AnonymousIdentity();
+		Mnemonic mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve);
+		IHDWallet<SampleWallet> _wallet = new TestHDWalletEd25519(mnemonic: mnemo.ToString(), "");
+		SampleWallet wallet = _wallet.GetAccount(0).GetExternalWallet(0);
+		ED25519PublicKey publicKey = new ED25519PublicKey(wallet.PublicKeyBytes);
+		var identity = new ED25519Identity(publicKey,wallet.PrivateKeyBytes);
 		var principal = identity.GetPrincipal();
 		GD.Print(principal.ToString());
+		
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
